3ee3d9872aa39f67ca2c6f38fa6c300b7e9a8c1c
nathan-mccraw <naterltw175@gmail.com>
2021-06-30 22:39:07 -0400

    Add Procfile.  Add port code in index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+app.listen(port);
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
