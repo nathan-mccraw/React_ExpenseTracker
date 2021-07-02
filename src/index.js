90338f23ad0b7035c6a9342d816bbb7651754405
nathan-mccraw <naterltw175@gmail.com>
2021-07-01 22:47:37 -0400

    delete procfile, delete port code lines in index.js

--- src/index.js
+++ src/index.js
@@ -4,11 +4,6 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 8000;
-}
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
